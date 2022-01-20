f69c3ceb196548351a2d7e3c5d908c3303548c89
vishal sam <vinishakr9@gmail.com>
2022-01-20 16:32:42 +0530

    have written the whole logic using switch case

--- src/com/employeedetails/employeedetails.java
+++ src/com/employeedetails/employeedetails.java
@@ -8,20 +8,24 @@ public class employeedetails {
 		int Is_Present=1;
 		int Is_Part_Time=2;
 		int Sal_Per_Hr=20;
-		int Work_Hr;
-		double emp = Math.floor(Math.random() * 10) %  3;
-		if (emp == 	Is_Present) {
-		System.out.println("Employee is present");
-		Work_Hr=8;
+		int Work_Hr=0;
+		int emp = (int) Math.floor(Math.random() * 10) %  3;
+		switch(emp) { 
+			case 1:
+				System.out.println("Employee is present");
+				Work_Hr=8;
+				break;
+			case 2:
+				System.out.println("Employee is parttimer");
+				Work_Hr=4;
+				break;
+			case 3:
+				System.out.println("Employee is absent");
+				Work_Hr=0;
+				break;
+			default:
+				System.out.println("contition doesnot exit");
 		}
-		else if (emp == Is_Part_Time) {
-			System.out.println("Employee is parttimer");
-			Work_Hr=4;
-		}
-		else{
-		System.out.println("Employee is absent");
-		Work_Hr=0;
-	}
 		int Daily_Wage= (Sal_Per_Hr * Work_Hr);
 		System.out.println("salary will be");
 		System.out.println(Daily_Wage);
