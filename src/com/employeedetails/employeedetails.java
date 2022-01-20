415a97c355a738c665bbae332eddb9d9165aaa90
vishal sam <vinishakr9@gmail.com>
2022-01-20 15:35:08 +0530

    checking is employee is fulltimeer or parttimer

--- src/com/employeedetails/employeedetails.java
+++ src/com/employeedetails/employeedetails.java
@@ -6,12 +6,17 @@ public class employeedetails {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 		int Is_Present=1;
+		int Is_Part_Time=2;
 		int Sal_Per_Hr=20;
 		int Work_Hr;
-		double emp = Math.floor(Math.random() * 10) %  2;
+		double emp = Math.floor(Math.random() * 10) %  3;
 		if (emp == 	Is_Present) {
 		System.out.println("Employee is present");
 		Work_Hr=8;
+		}
+		else if (emp == Is_Part_Time) {
+			System.out.println("Employee is parttimer");
+			Work_Hr=4;
 		}
 		else{
 		System.out.println("Employee is absent");
