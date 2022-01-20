c0cfccde108ac047c834b80a592ecdc41d57e537
vishal sam <vinishakr9@gmail.com>
2022-01-20 14:53:46 +0530

    Added employee attedance

---
+++ src/com/employeedetails/employeedetails.java
@@ -0,0 +1,17 @@
+package com.employeedetails;
+
+public class employeedetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+		int Is_Full_Time=1;
+		double emp = Math.floor(Math.random() * 10) %  2;
+		if (emp == 	Is_Full_Time)
+		System.out.println("Employee is present");
+		else
+		System.out.println("Employee is absent");
+
+	}
+
+}
