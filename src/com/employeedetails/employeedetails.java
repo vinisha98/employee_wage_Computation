1d56e9d6472d48a6678151bff6910bb4be57a065
vishal sam <vinishakr9@gmail.com>
2022-01-20 15:23:07 +0530

    Added per day salary calculation

--- src/com/employeedetails/employeedetails.java
+++ src/com/employeedetails/employeedetails.java
@@ -5,13 +5,20 @@ public class employeedetails {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-		int Is_Full_Time=1;
+		int Is_Present=1;
+		int Sal_Per_Hr=20;
+		int Work_Hr;
 		double emp = Math.floor(Math.random() * 10) %  2;
-		if (emp == 	Is_Full_Time)
+		if (emp == 	Is_Present) {
 		System.out.println("Employee is present");
-		else
+		Work_Hr=8;
+		}
+		else{
 		System.out.println("Employee is absent");
-
+		Work_Hr=0;
 	}
-
+		int Daily_Wage= (Sal_Per_Hr * Work_Hr);
+		System.out.println("salary will be");
+		System.out.println(Daily_Wage);
+	}
 }
