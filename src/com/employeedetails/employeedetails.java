8811a41c3166580d9bcbef8b9b0d6944912c8e35
vinisha98 <96873814+vinisha98@users.noreply.github.com>
2022-01-20 20:38:03 +0530

    Update employeedetails.java

--- src/com/employeedetails/employeedetails.java
+++ src/com/employeedetails/employeedetails.java
@@ -1,11 +1,14 @@
 package com.employeedetails;
 
 public class employeedetails {
+	public static final  int Is_Present=1;
+		public static final int Is_Part_Time=2;
+			public static final int Sal_Per_Hr=20;
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-		int Sal_Per_Hr=20;
 		int Emp_Hr=0;
 		int Total = 0;
 		int Days = 0;
@@ -29,16 +32,14 @@ public class employeedetails {
 			default:
 				System.out.println("contition doesnot exit");
 		}
-		System.out.println("total working days will be");
-		System.out.println(Days);
-		System.out.println("total working hours will be");
-		System.out.println(Work_Hr);
-		int Daily_Wage= (Sal_Per_Hr * Emp_Hr);
+			int Daily_Wage= (Sal_Per_Hr * Emp_Hr);
 		Total= (Total+Daily_Wage);
 		Days=(Days+1);
 		Work_Hr=(Work_Hr+Emp_Hr);
-		System.out.println("totalsalary will be");
-		System.out.println(Total);
 		}
+		System.out.println("totalsalary will be: " + Total);
+		System.out.println(Total);
+		System.out.println("total working days will be: " + Days);
+		System.out.println("total working hours will be: " + Work_Hr);
 	}
 }
