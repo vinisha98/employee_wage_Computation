40650dee3989fbc9bbe267fd57751bb53c0e1616
vishal sam <vinishakr9@gmail.com>
2022-01-20 17:56:46 +0530

    calculated monthly wage

--- src/com/employeedetails/employeedetails.java
+++ src/com/employeedetails/employeedetails.java
@@ -6,26 +6,37 @@ public class employeedetails {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 		int Sal_Per_Hr=20;
-		int Work_Hr=0;
+		int Emp_Hr=0;
+		int Total = 0;
+		int Days = 0;
+		int Work_Hr = 0;
+		while(Days<=20)
+		{
 		int emp = (int) Math.floor(Math.random() * 10) %  3;
 		switch(emp) { 
 			case 1:
 				System.out.println("Employee is present");
-				Work_Hr=8;
+				Emp_Hr=8;
 				break;
 			case 2:
 				System.out.println("Employee is parttimer");
-				Work_Hr=4;
+				Emp_Hr=4;
 				break;
 			case 3:
 				System.out.println("Employee is absent");
-				Work_Hr=0;
+				Emp_Hr=0;
 				break;
 			default:
 				System.out.println("contition doesnot exit");
 		}
-		int Daily_Wage= (Sal_Per_Hr * Work_Hr);
-		System.out.println("salary will be");
-		System.out.println(Daily_Wage);
+		int Daily_Wage= (Sal_Per_Hr * Emp_Hr);
+		Total= (Total+Daily_Wage);
+		Days=(Days+1);
+		Work_Hr=(Work_Hr+Emp_Hr);
+		}
+		System.out.println("totalsalary will be");
+		System.out.println(Total);
+		System.out.println("total working hours will be");
+		System.out.println(Work_Hr);	
 	}
 }
